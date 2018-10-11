180d10896e176d900b2044e65fe040a6f7076675
Alena Zueva <vavilova.alena@bk.ru>
2018-10-11 09:17:52 +0500

    Refactoring

--- TZ/Form1.cs
+++ TZ/Form1.cs
@@ -13,12 +13,6 @@ namespace TZ
 {
     public partial class Form1 : Form
     {
-       
-        int i;
-        int control_number = 0;
-         
-
-
         public Form1()
         {
             InitializeComponent();
@@ -27,34 +21,44 @@ namespace TZ
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-                i = 1;
-                string text1 = textBox1.Text;
-                if (text1 != "")
+
+            string text1 = textBox1.Text;
+
+            if (text1 is null || text1.Length <= 0)
+            {
+                MessageBox.Show("Пустое поле ввода!");
+                return;
+            }
+            int control_number = countWordsStartWithT(text1);
+
+            if (control_number == 0)
+            {
+                MessageBox.Show("Нет слов начинающихся с буквы Т!");
+            }
+            else
+            {
+                MessageBox.Show("Количество слов начинающихся с буквы Т = " + control_number.ToString());
+            }
+        }
+
+        private static int countWordsStartWithT(string text1)
+        {
+            int control_number = 0;
+            if (((text1[0] == 'T') || (text1[0] == 't') || (text1[0] == 'Т') || (text1[0] == 'т')))
+            {
+                control_number++;
+            }
+
+
+            for (int i = 1; i < text1.Length; i++)
+            {
+                if ((text1[i - 1] == ' ') && ((text1[i] == 'T') || (text1[i] == 't') || (text1[i] == 'Т') || (text1[i] == 'т')))
                 {
-
-                if (((text1[0] == 'T') || (text1[0] == 't') || (text1[0] == 'Т') || (text1[0] == 'т')))
-                { control_number++; }
-
-
-                for (i = 1; i < text1.Length; i++)
-                    {
-                        if ((text1[i - 1] == ' ') && ((text1[i] == 'T') || (text1[i] == 't') || (text1[i] == 'Т') || (text1[i] == 'т')))
-                        {
-                            control_number++;
-                        }
-                    }
-
-                   
-                    if (control_number == 0)
-                        { MessageBox.Show("Нет слов начинающихся с буквы Т!"); }
-                    else
-                        { MessageBox.Show("Количество слов начинающихся с буквы Т = " + control_number.ToString()); }
-
+                    control_number++;
                 }
-                else { MessageBox.Show("Пустое поле ввода!"); }
+            }
 
-            
+            return control_number;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
